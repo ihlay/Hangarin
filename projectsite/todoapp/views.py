c9b56f8391787bf2c448de84d8ab2df1b44e8b6c
ihlay <arnejoelikarl@gmail.com>
2026-03-22 14:21:47 +0800

    Add all views of Task to views.py

--- projectsite/todoapp/views.py
+++ projectsite/todoapp/views.py
@@ -30,3 +30,114 @@ class HomePageView(LoginRequiredMixin, ListView):
         )
         context['recent_tasks'] = Task.objects.select_related('category', 'priority').order_by('-created_at')[:5]
         return context
+
+
+
+class TaskListView(LoginRequiredMixin, ListView):
+    model = Task
+    context_object_name = 'task'
+    template_name = 'task_list.html'
+    paginate_by = 5
+
+    def get_queryset(self):
+        qs = super().get_queryset().select_related('category', 'priority').order_by('-created_at')
+
+        query = self.request.GET.get('q')
+        if query:
+            qs = qs.filter(
+                Q(title__icontains=query) |
+                Q(description__icontains=query)
+            )
+
+        status = self.request.GET.get('status')
+        if status:
+            qs = qs.filter(status=status)
+
+        priority_id = self.request.GET.get('priority')
+        if priority_id:
+            qs = qs.filter(priority__id=priority_id)
+
+        category_id = self.request.GET.get('category')
+        if category_id:
+            qs = qs.filter(category__id=category_id)
+
+        return qs
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['priorities'] = Priority.objects.all()
+        context['categories'] = Category.objects.all()
+        context['status_choices'] = Task.STATUS_CHOICES
+        return context
+
+
+class TaskCreateView(LoginRequiredMixin, CreateView):
+    model = Task
+    form_class = TaskForm
+    template_name = 'task_form.html'
+    success_url = reverse_lazy('task-list')
+
+
+class TaskUpdateView(LoginRequiredMixin, UpdateView):
+    model = Task
+    form_class = TaskForm
+    template_name = 'task_form.html'
+    success_url = reverse_lazy('task-list')
+
+
+class TaskDeleteView(LoginRequiredMixin, DeleteView):
+    model = Task
+    template_name = 'task_del.html'
+    success_url = reverse_lazy('task-list')
+
+
+
+class SubTaskListView(LoginRequiredMixin, ListView):
+    model = SubTask
+    context_object_name = 'subtask'
+    template_name = 'subtask_list.html'
+    paginate_by = 5
+
+    def get_queryset(self):
+        qs = super().get_queryset().select_related('parent_task')
+
+        query = self.request.GET.get('q')
+        if query:
+            qs = qs.filter(
+                Q(title__icontains=query) |
+                Q(parent_task__title__icontains=query)
+            )
+
+        status = self.request.GET.get('status')
+        if status:
+            qs = qs.filter(status=status)
+
+        return qs
+
+    def get_ordering(self):
+        return 'title'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['status_choices'] = SubTask.STATUS_CHOICES
+        return context
+
+
+class SubTaskCreateView(LoginRequiredMixin, CreateView):
+    model = SubTask
+    form_class = SubTaskForm
+    template_name = 'subtask_form.html'
+    success_url = reverse_lazy('subtask-list')
+
+
+class SubTaskUpdateView(LoginRequiredMixin, UpdateView):
+    model = SubTask
+    form_class = SubTaskForm
+    template_name = 'subtask_form.html'
+    success_url = reverse_lazy('subtask-list')
+
+
+class SubTaskDeleteView(LoginRequiredMixin, DeleteView):
+    model = SubTask
+    template_name = 'subtask_del.html'
+    success_url = reverse_lazy('subtask-list')
